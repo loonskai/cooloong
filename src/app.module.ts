092810cc09db47ebbde892646d67c0746587462e
Siarhei Lunski <loonskai@gmail.com>
2021-08-07 19:31:28 +0200

    Generate project

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { join } from 'path';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot({
+      playground: true,
+      typePaths: ['./**/*.graphql'],
+      definitions: {
+        path: join(process.cwd(), 'src/graphql.ts'),
+      },
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
